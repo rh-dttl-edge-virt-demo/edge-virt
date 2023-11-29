0400443b4d252f04861a059b215647aafccb97a8
James Harmison <jharmison@redhat.com>
2023-11-29 12:36:26 -0500

    Fix whitespace problems

--- applications/provision/files/update-agents.sh
+++ applications/provision/files/update-agents.sh
@@ -10,7 +10,7 @@ patched_agents=()
 # Until we've patched all hosts
 while ((${#hosts[@]} != ${#patched_agents[@]})); do
 	# Iterate through the hosts
-	for host in "${hosts[@]}"; do
+	for host in ${hosts[*]}; do
 		# Identify if an agent has been created
 		agent="$(oc get agent -ogo-template='{{ range .items }}{{ if eq .status.inventory.hostname "'"$host"'" }}{{ .metadata.name }}{{ end }}{{ end }}')"
 		# If an agent has been created and we haven't already patched it
