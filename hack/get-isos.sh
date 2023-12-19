92344106c0b1848b2a71d70aa015d198da2186d4
James Harmison <jharmison@redhat.com>
2023-12-19 14:04:29 -0500

    Implement continuous retries in get-isos

--- hack/get-isos.sh
+++ hack/get-isos.sh
@@ -6,28 +6,38 @@ export KUBECONFIG="${PWD}/install/auth/kubeconfig"
 is_iso() {
 	file "$1" | grep -qF 'ISO 9660'
 }
+isos_exist() {
+	compgen -G "install/*.iso" >/dev/null
+}
 
 ret=0
-while read -r namespace name; do
-	iso="install/$name.iso"
-	if [ -e "$iso" ] && is_iso "$iso"; then
-		echo "Already have $name discovery ISO"
-		continue
-	fi
-	echo -n "Downloading $name discovery ISO"
-	created=$(bin/oc get -n "$namespace" infraenv "$name" -ogo-template='{{ range .status.conditions }}{{ if eq .type "ImageCreated" }}{{ .status }}{{ end }}{{ end }}')
-	while [ "$created" != "True" ]; do
-		echo -n '.'
-		sleep 1
-	done
-	echo
-	url=$(bin/oc get -n "$namespace" infraenv "$name" -ogo-template='{{ .status.isoDownloadURL }}')
-	curl -kLo "$iso" "$url"
-	if ! is_iso "$iso"; then
-		rm -f "$iso"
-		echo "Error downloading discovery ISO for $name, please investigate" >&2
-		((ret += 1))
-	fi
-done < <(bin/oc get infraenv -Aogo-template='{{ range .items }}{{ .metadata.namespace }} {{ .metadata.name }}{{ "\n" }}{{ end }}')
+
+echo -n "Waiting for API to respond with discovery iso name"
+while ! isos_exist && ((ret == 0)); do
+	echo -n '.'
+	while read -r namespace name; do
+		echo
+		iso="install/$name.iso"
+		if [ -e "$iso" ] && is_iso "$iso"; then
+			echo "Already have $name discovery ISO"
+			continue
+		fi
+		echo -n "Downloading $name discovery ISO"
+		created=$(bin/oc get -n "$namespace" infraenv "$name" -ogo-template='{{ range .status.conditions }}{{ if eq .type "ImageCreated" }}{{ .status }}{{ end }}{{ end }}')
+		while [ "$created" != "True" ]; do
+			echo -n '.'
+			sleep 1
+		done
+		echo
+		url=$(bin/oc get -n "$namespace" infraenv "$name" -ogo-template='{{ .status.isoDownloadURL }}')
+		curl -kLo "$iso" "$url"
+		if ! is_iso "$iso"; then
+			rm -f "$iso"
+			echo "Error downloading discovery ISO for $name, please investigate" >&2
+			((ret += 1))
+		fi
+	done < <(bin/oc get infraenv -Aogo-template='{{ range .items }}{{ .metadata.namespace }} {{ .metadata.name }}{{ "\n" }}{{ end }}')
+	if ! isos_exist; then sleep 5; fi
+done
 
 exit $ret
